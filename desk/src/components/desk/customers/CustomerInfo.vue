9ab82aaec63ccb4a0e6fd23c98cf4a7eae597afe
fadilsid <fadil@tridz.com>
2022-11-29 12:23:26 +0000

    ui fixes and logical corrections

--- desk/src/components/desk/customers/CustomerInfo.vue
+++ desk/src/components/desk/customers/CustomerInfo.vue
@@ -1,219 +1,230 @@
 <template>
 	<div class="flex flex-col">
-			<div class="customer-info flex-1">
-				<div class="flex flex-row">
-					<div class="info basis-1/2 m-[20px]">
-						<div v-if="editingTitle">
-							<div>
-								<Input
-									label="Customer Name"
-									type="text"
-									v-model="customer"
-								/>
-							</div>
-							<div>
-								<Input
-									label="Domain"
-									type="text"
-									v-model="values.domain"
-								/>
-							</div>
+		<div class="customer-info flex-1">
+			<div class="flex flex-row">
+				<div class="info basis-1/2 m-[20px]">
+					<div v-if="editingTitle">
+						<div class="max-w-max mb-[10px]">
+							<Input
+								label="Customer Name"
+								type="text"
+								v-model="customer"
+							/>
 						</div>
-						<div v-else>
-							<div class="flex">
-								<div
-									class="bg-[#90C5F4] w-14 h-14 justify-center flex items-center rounded-md font-medium text-white text-2xl"
-								>
-									<span>{{ acronym[0] }} {{ acronym[1] }}</span>
+						<div class="max-w-max">
+							<Input
+								label="Domain"
+								type="text"
+								v-model="values.domain"
+							/>
+						</div>
+					</div>
+					<div v-else>
+						<div class="flex">
+							<div
+								class="bg-[#90C5F4] w-14 h-14 justify-center flex items-center rounded-md font-medium text-white text-2xl"
+							>
+								<span>{{ acronym[0] }} {{ acronym[1] }}</span>
+							</div>
+							<div class="pl-4">
+								<div class="font-medium text-2xl w-full">
+									{{ values.customerName }}
 								</div>
-								<div class="pl-4">
-									<div class="font-medium text-2xl w-full">
-										{{ values.customerName }}
-									</div>
 
-									<div class="font-light text-base">
-										{{ values.domain }}
-									</div>
+								<div class="font-light text-base">
+									{{ values.domain }}
 								</div>
 							</div>
 						</div>
 					</div>
-					
-					<div class="actions basis-1/2 m-[20px]">
-						<div v-if="editingTitle" class="flex flex-row justify-end">
-							<Button
-								class="mr-1"
-								appearance="primary"
-								@click="
-									() => {
-										updateCustomer()
-										editingTitle = false
-									}
-								"
-								>Save</Button
-							>
-							<Button
-								class="mr-1"
-								appearance="secondary"
-								@click="
-									() => {
-										editingTitle = false
-										$resources.customer.reload()
-									}
-								"
-								>Discard</Button
-							>
-						</div>
-						<div v-else class="flex flex-row justify-end">
-							<Button
-								class="mr-1"
-								appearance="secondary"
-								icon-left="edit"
-								@click="editingTitle = true"
-								>Edit</Button
-							>
-							<Button
-								class="mr-1"
-								appearance="secondary"
-								icon-left="trash"
-								@click="deleteCustomer()"
-								>Delete</Button
-							>
-							<Button
-								class="mr-1"
-								appearance="secondary"
-								icon-left="plus"
-								@click="
-									() => {
-										showNewContactDialog = true
-									}
-								"
-								>Add Contact</Button
-							>
-							<Button
-								class="mr-1"
-								appearance="secondary"
-								icon-left="plus"
-								@click="
-									() => {
-										showNewTicketDialog = true
-									}
-								"
-								>Add Ticket</Button
-							>
-						</div>
-					</div>
-				</div>	
-			</div>
+				</div>
 
-			<div class="customer-table flex-1">
-				<AccordionCustomer class="customer-accordion flex flex-col">
-					<template v-slot:title>
-						<span class="font-medium text-lg"
-							>Contacts ({{
-								customerDoc.contact_count == null
-									? "0"
-									: customerDoc.contact_count
-							}})
-						</span>
-					</template>
-
-					<template v-slot:contact>
-						<div
-							v-for="contact in contactDoc"
-							class="flex font-normal text-sm py-2 items-center space-x-3 mx-5 py-2 px-4 border-b border-[#F4F5F6]-200"							
+				<div class="actions basis-1/2 m-[20px]">
+					<div v-if="editingTitle" class="flex flex-row justify-end">
+						<Button
+							class="mr-1"
+							appearance="primary"
+							@click="
+								() => {
+									updateCustomer()
+									editingTitle = false
+								}
+							"
+							>Save</Button
 						>
-							<div class="w-[30%]">
-								<router-link
-									:to="`/frappedesk/contacts/${contact.name}`"
-								>
-									{{ contact.name }}
-								</router-link>
-							</div>
-							<div class="w-[20%]">
-								{{ contact.email_ids[0].email_id }}
-							</div>
-							<div class="w-[20%]">
+						<Button
+							class="mr-1"
+							appearance="secondary"
+							@click="
+								() => {
+									editingTitle = false
+									$resources.customer.reload()
+								}
+							"
+							>Discard</Button
+						>
+					</div>
+					<div v-else class="flex flex-row justify-end">
+						<Button
+							class="mr-1"
+							appearance="secondary"
+							icon-left="edit"
+							@click="editingTitle = true"
+							>Edit</Button
+						>
+						<Button
+							class="mr-1"
+							appearance="secondary"
+							icon-left="trash"
+							@click="deleteCustomer()"
+							>Delete</Button
+						>
+						<Button
+							class="mr-1"
+							appearance="secondary"
+							icon-left="plus"
+							@click="
+								() => {
+									showNewContactDialog = true
+								}
+							"
+							>Add Contact</Button
+						>
+						<Button
+							class="mr-1"
+							appearance="secondary"
+							icon-left="plus"
+							@click="
+								() => {
+									showNewTicketDialog = true
+								}
+							"
+							>Add Ticket</Button
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="customer-table flex-1">
+			<AccordionCustomer class="customer-accordion flex flex-col">
+				<template v-slot:title>
+					<span class="font-medium text-lg flex-1"
+						>Contacts ({{
+							customerDoc.contact_count == null
+								? "0"
+								: customerDoc.contact_count
+						}})
+					</span>
+				</template>
+
+				<template v-slot:data>
+					<div
+						v-for="contact in contactDoc"
+						class="flex font-normal text-sm py-2 items-center space-x-3 mx-5 py-2 px-4 border-b border-[#F4F5F6]-200"
+					>
+						<div class="w-[30%]">
+							<router-link
+								:to="`/frappedesk/contacts/${contact.name}`"
+							>
+								{{ contact.first_name }}
 								{{
-									contact.phone_nos.length == 0
+									contact.last_name == null
 										? ""
-										: contact.phone_nos[0].phone
+										: contact.last_name
 								}}
-							</div>
+							</router-link>
 						</div>
-					</template>
-				</AccordionCustomer>
-			</div>
-			<div class="ticket-table flex-1 mt-5">
-				<AccordionCustomer class="ticket-accordion flex flex-col">
-					<template v-slot:title>
-						<span class="font-medium text-lg"
-							>Tickets ({{
-								customerDoc.ticket_count == null
-									? "0"
-									: customerDoc.ticket_count
-							}})
-						</span>
-					</template>
+						<div class="w-[20%]">
+							{{ contact.email_ids[0].email_id }}
+						</div>
+						<div class="w-[20%]">
+							{{
+								contact.phone_nos.length == 0
+									? ""
+									: contact.phone_nos[0].phone
+							}}
+						</div>
+					</div>
+				</template>
+			</AccordionCustomer>
+		</div>
+		<div class="ticket-table flex-1 mt-5">
+			<AccordionCustomer class="flex flex-col ticket-accordion">
+				<template v-slot:title>
+					<span class="flex-1 font-medium text-lg"
+						>Tickets ({{
+							customerDoc.ticket_count == null
+								? "0"
+								: customerDoc.ticket_count
+						}})
+					</span>
+				</template>
 
-					<template v-slot:ticket>
-						<div
-							v-for="ticket in ticketDoc"
-							class="flex w-[100%] pl-[22px] font-normal text-sm py-3 mx-5"
-						>
-							<div
-								class="font-normal w-[10%] text-sm text-[#74808B]"
+				<template v-slot:data>
+					<div
+						v-for="ticket in ticketDoc"
+						class="flex w-[100%] pl-[22px] font-normal text-sm py-3 mx-5"
+					>
+						<div class="font-normal w-[10%] text-sm text-[#74808B]">
+							{{ ticket.name }}
+						</div>
+
+						<div class="w-[40%] font-normal text-sm text-[#192734]">
+							<router-link
+								:to="`/frappedesk/tickets/${ticket.name}`"
 							>
-								{{ ticket.name }}
-							</div>
+								{{ ticket.subject }}
+							</router-link>
+						</div>
 
-							<div
-								class="w-[40%] font-normal text-sm text-[#192734]"
-							>
-								<router-link
-									:to="`/frappedesk/tickets/${ticket.name}`"
-								>
-									{{ ticket.subject }}
-								</router-link>
-							</div>
+						<div class="w-[10%] font-normal text-xs text-[#74808B]">
+							<span :class="getStatusStyle(ticket.status)">{{
+								ticket.status
+							}}</span>
+						</div>
 
-							<div
-								class="w-[10%] font-normal text-xs text-[#74808B]"
-								:class="getStatusStyle(ticket.status)"
-							>
-								{{ ticket.status }}
-							</div>
-
-							<div
-								class="w-[10%] font-medium text-xs text-[#74808B]"
+						<div class="w-[10%] font-medium text-xs text-[#74808B]">
+							<span
+								class="rounded-[48px]"
 								:class="getTypeStyle(ticket.ticket_type)"
 							>
 								{{ ticket.ticket_type }}
-							</div>
-
-							<div
-								class="w-[10%] font-normal text-xs text-[#74808B]"
-							>
-								{{ ticket.priority }}
-							</div>
-
-							<div
-								class="w-[10%] font-normal text-sm text-[#74808B]"
-							>
-								{{ ticket.contact }}
-							</div>
-
-							<!-- <Avatar
-									v-for="contact in contactDoc"
-									:label="user"
-									:imageURL="contact.image"
-									size="md"
-								/> -->
+							</span>
 						</div>
-					</template>
-				</AccordionCustomer>
-			</div>
-		
+
+						<div class="w-[10%] font-normal text-xs text-[#74808B]">
+							{{ ticket.priority }}
+						</div>
+
+						<div
+							v-for="contact in contactDoc"
+							class="w-[10%] font-normal text-sm text-[#74808B]"
+						>
+							{{
+								contact.name == ticket.contact
+									? contact.first_name +
+									  " " +
+									  contact.last_name
+									: ""
+							}}
+						</div>
+						<!-- <div class="w-[10%]" v-for="contact in contactDoc">
+							<Avatar
+								:label="user"
+								class="w-[10%]"
+								:imageURL="
+									contact.name == ticket.contact
+										? contact.image
+										: ''
+								"
+								size="md"
+							/>
+						</div> -->
+					</div>
+				</template>
+			</AccordionCustomer>
+		</div>
 
 		<NewContactDialog
 			v-model="showNewContactDialog"
@@ -371,22 +382,21 @@ export default {
 			}
 		},
 		getStatusStyle(status) {
-			const color = {
-				Open: "#38A160",
-				Replied: "#FF7C36",
-				Resolved: "#E24C4C",
-				Closed: "#E24C4C",
+			const badge = {
+				Open: "badge-success",
+				Resolved: "badge-danger",
+				Closed: "badge-danger",
 			}[status]
-			return `border-[${color}] text-[${color}]`
+			return badge
 		},
 
 		getTypeStyle(ticket_type) {
-			const color = {
-				Question: "#B7B6FC",
-				Bug: "#E24C4C",
-				Incident: "#FF7C36",
+			const badge = {
+				Question: "badge-question",
+				Bug: "badge-danger",
+				Incident: "badge-incident",
 			}[ticket_type]
-			return `border-[${color} text-[${color}]]`
+			return badge
 		},
 	},
 }
